26acff6afe9ded8cc209bc67413ff55d0f5e0892
j.faassen <j.faassen@linkorb.com>
2017-02-23 22:16:00 +0100

    PropertyTrait enhancements

--- src/Model/PropertyTrait.php
+++ src/Model/PropertyTrait.php
@@ -2,36 +2,45 @@
 
 namespace Realm\Model;
 
+use RuntimeException;
+
 trait PropertyTrait
 {
     protected $properties = [];
-    
+
     public function addProperty(Property $property)
     {
         $this->properties[$property->getName()] = $property;
         return $this;
     }
-    
+
     public function getProperties()
     {
         return $this->properties;
     }
-    
-    public function hasProperty($name)
+
+    public function hasProperty($language, $name)
     {
-        return isset($this->properties[$name]);
-    }
-    
-    public function getProperty($name)
-    {
-        if (!$this->hasProperty($name)) {
-            throw new RuntimeException("No such property");
+        foreach ($this->properties as $property) {
+            if (($property->getLanguage()==$language) && ($property->getName()==$name)) {
+                return true;
+            }
         }
-        return $this->properties[$name];
+        return false;
     }
-    
-    public function getPropertyValue($name)
+
+    public function getProperty($language, $name)
     {
-        return $this->getProperty($name)->getValue();
+        foreach ($this->properties as $property) {
+            if (($property->getLanguage()==$language) && ($property->getName()==$name)) {
+                return $property;
+            }
+        }
+        throw new RuntimeException("No such property: $language/$name");
+    }
+
+    public function getPropertyValue($language, $name)
+    {
+        return $this->getProperty($language, $name)->getValue();
     }
 }
