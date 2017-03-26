5258213963a7974ab3ac2b212f366f37e871257d
j.faassen <j.faassen@linkorb.com>
2017-03-26 07:57:31 +0200

    css/js enhancements
    - highlight selected nav options
    - select first nav option on document load

--- web/realm.js
+++ web/realm.js
@@ -19,7 +19,14 @@ $( document ).ready(function() {
       $(this).removeClass('realm-value-hover')
     })
   });
-
+  var selected=false;
+  $(".detail").each(function(index) {
+    //console.log(this.id);
+    if (!selected) {
+      selectDetail(this.id);
+      selected = true;
+    }
+  })
 });
 
 function selectDetail(detailId) {
@@ -27,5 +34,9 @@ function selectDetail(detailId) {
   $(".detail").each(function(index) {
     $(this).removeClass('detail-active')
   })
+  $(".nav-detail").each(function(index) {
+    $(this).removeClass('nav-detail-active')
+  })
   $('#' + detailId).addClass('detail-active');
+  $('.nav-' + detailId).addClass('nav-detail-active');
 }
