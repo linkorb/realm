cb40c3805be5c9710a59a167a14584239c18052a
boite <boite@users.noreply.github.com>
2018-02-15 12:24:45 +0000

    Add TerminologyEditorCommand

--- app/config/cli-services.php
+++ app/config/cli-services.php
@@ -6,6 +6,10 @@
  */
 
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+use Realm\Command\TerminologyEditorCommand;
 
 // Service Definition template
 $definition = new Definition();
@@ -15,6 +19,33 @@ $definition
     ->setPublic(false)
 ;
 
+// container params
+$container->setParameter('default_terminology_editor_language_code', 'nl_NL');
+$container->setParameter(
+    'contextual_concept_attributes',
+    [
+        'shortName',
+        'oid',
+        'type',
+        'status',
+        'codelist.displayName',
+        'codelist.id',
+    ]
+);
+
+// libs
+$container->register(PropertyAccessor::class, PropertyAccessor::class)
+    ->setFactory([PropertyAccess::class, 'createPropertyAccessor'])
+;
+
+// application services
+$this->registerClasses($definition, 'Realm\\Loader\\', '../../src/Loader');
+
 // application commands (which need to be public services)
 $definition->setPublic(true);
 $this->registerClasses($definition, 'Realm\\Command\\', '../../src/Command');
+$container
+    ->getDefinition(TerminologyEditorCommand::class)
+    ->setArgument('$defaultLanguageCode', '%default_terminology_editor_language_code%')
+    ->setArgument('$contextualConceptAttributes', '%contextual_concept_attributes%')
+;
---
+++ src/Command/TerminologyEditorCommand.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Realm\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+use Realm\Loader\XmlRealmLoader;
+use Realm\Model\Project;
+
+class TerminologyEditorCommand extends Command
+{
+    protected static $defaultName = 'terminology:editor';
+
+    private $contextualConceptAttributes;
+    private $defaultLanguageCode;
+    private $propertyAccessor;
+    private $realmLoader;
+
+    public function __construct(
+        XmlRealmLoader $realmLoader,
+        PropertyAccessor $propertyAccessor,
+        $defaultLanguageCode,
+        $contextualConceptAttributes,
+        $name = null
+    ) {
+        $this->realmLoader = $realmLoader;
+        $this->propertyAccessor = $propertyAccessor;
+        $this->defaultLanguageCode = $defaultLanguageCode;
+        $this->contextualConceptAttributes = $contextualConceptAttributes;
+        parent::__construct($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Prepare a file of terminilogy for editors')
+            ->addArgument(
+                'realm',
+                InputArgument::REQUIRED,
+                'The name of a realm project (e.g. "peri22xx") which will be located immediately below REALM_PATH'
+            )
+            ->addArgument(
+                'property',
+                InputArgument::REQUIRED,
+                'The name of a concept property (either existing or not) to be edited'
+            )
+            ->addOption(
+                'language',
+                'l',
+                InputOption::VALUE_OPTIONAL,
+                'Select properties in the given language',
+                $this->defaultLanguageCode
+            )
+            ->addOption(
+                'context',
+                'c',
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'The name of a concept property which provides context to assist with editing'
+            )
+            ->addOption(
+                'csv',
+                null,
+                InputOption::VALUE_NONE,
+                'Output comma separated values instead of tab separated values'
+            )
+        ;
+        if (false !== $dir = getcwd()) {
+            $this->addArgument(
+                'path',
+                InputArgument::OPTIONAL,
+                'Path to which to write the terminology file (use "-" for stdout)',
+                $dir
+            );
+        } else {
+            $this->addArgument(
+                'path',
+                InputArgument::REQUIRED,
+                'Path to which to write the terminology file (use "-" for stdout)'
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $propertyToEdit = $input->getArgument('property');
+        $editorLanguage = $input->getOption('language');
+        $outputToStdOut = '-' === $input->getArgument('path');
+        $outputFileExtention = 'tsv';
+        $outputDelimiter = "\t";
+
+        if ($input->getOption('csv')) {
+            $outputFileExtention = 'csv';
+            $outputDelimiter = ',';
+        }
+        $fileHandle = null;
+        if (!$outputToStdOut) {
+            $fileHandle = fopen(
+                "{$input->getArgument('path')}/{$propertyToEdit}.{$editorLanguage}.{$outputFileExtention}",
+                'wb'
+            );
+        } else {
+            $fileHandle = fopen('php://stdout', 'wb');
+        }
+
+        $project = new Project();
+        $this->realmLoader->load($input->getArgument('realm'), $project);
+
+        $header = ['CONCEPT', strtoupper($propertyToEdit)];
+        foreach ($input->getOption('context') as $propertyName) {
+            $header[] = strtoupper($propertyName);
+        }
+        foreach ($this->contextualConceptAttributes as $attrName) {
+            $header[] = strtoupper($attrName);
+        }
+        fputcsv($fileHandle, $header, $outputDelimiter);
+
+        foreach ($project->getConcepts() as $id => $concept) {
+            $row = [$id];
+            if ($concept->hasProperty($propertyToEdit, $editorLanguage)) {
+                $row[] = $concept->getPropertyValue($propertyToEdit, $editorLanguage);
+            } else {
+                $row[] = '';
+            }
+            foreach ($input->getOption('context') as $propertyName) {
+                if ($concept->hasProperty($propertyName, $editorLanguage)) {
+                    $row[] = $concept->getPropertyValue($propertyName, $editorLanguage);
+                } else {
+                    $row[] = '';
+                }
+            }
+            foreach ($this->contextualConceptAttributes as $attrPath) {
+                if (!$this->propertyAccessor->isReadable($concept, $attrPath)) {
+                    $row[] = '';
+                    continue;
+                }
+                $attrValue = $this->propertyAccessor->getValue($concept, $attrPath);
+                $row[] = null === $attrValue ? '' : $attrValue;
+            }
+            fputcsv($fileHandle, $row, $outputDelimiter);
+        }
+
+        if (!$outputToStdOut) {
+            fclose($fileHandle);
+        }
+    }
+}
