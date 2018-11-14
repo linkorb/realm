71c6675227315642f387627cc4a699477e10560c
j.faassen <j.faassen@linkorb.com>
2018-11-14 15:59:59 +0100

    Enhance debugging with debug toggle and visor updates

--- src/Presenter/FusionPresenter.php
+++ src/Presenter/FusionPresenter.php
@@ -33,26 +33,6 @@ class FusionPresenter extends BasePresenter
 
         $visor = '<div class="realm-visor">';
 
-        $visor .= '<div class="realm-debug">';
-        $visor .= 'Concept: ' . $conceptId;
-        $concept = $this->getConcept($conceptId);
-        if ($concept) {
-            $visor .= ' (' . $concept->getShortName() . ')';
-        }
-
-        $visor .= '<br />';
-        foreach ($values as $value) {
-            if ($value) {
-                $visor .= 'Value: ' . $value->getValue() . '<br />';
-                $visor .= 'Display: ' . $value->getPresenter()->getDisplayValue() . '<br />';
-            } else {
-                $visor .= 'Value: null<br />';
-            }
-            $visor .= '<hr />';
-        }
-
-        $visor .= '</div>';
-
         $visor .= '<table class="table" style="width: 400px;">';
         $visor .= '<tr>';
         $visor .= '<th colspan="2">Bron</th>';
@@ -62,20 +42,45 @@ class FusionPresenter extends BasePresenter
 
         foreach ($values as $value) {
             $visor .= '<tr>';
-            $visor .= '<td style="white-space: nowrap;" colspan="2">' . $value->getSection()->getResource()->getSource()->getDisplayName();
-
-            $visor .= '<tr>';
-            $visor .= '</tr>';
-            $visor .= '<td style="width: 80px;">';
-            $visor .= '<img src="' . $value->getSection()->getResource()->getSource()->getLogoUrl() . '" style="margin-right: 3px;"/>';
-            $visor .= '<img src="' . $value->getSection()->getResource()->getSource()->getAppLogoUrl() . '" />';
-            $visor .= '<br /><small><span class="date" style="white-space: nowrap;">' . $value->getSection()->getPresenter()->getEffectiveAt() . '</span></small><br />';
+            $visor .= '<td style="width: 50px;">';
+            $visor .= '<div class="logo-group">';
+            $visor .= '<img src="' . $value->getSection()->getResource()->getSource()->getLogoUrl() . '" class="logo logo-provider" />';
+            $visor .= '<img src="' . $value->getSection()->getResource()->getSource()->getAppLogoUrl() . '" class="logo logo-app" />';
+            $visor .= '</div>';
             $visor .= '</td>';
-            $visor .= '<td><b style="border: 1px solid; display: block; padding: 3px; border-radius: 2px; background-color: #ffffff;">' . $value->getPresenter()->getDisplayValue() . '</b>';
-            $visor .= '</td>';
+            $visor .= '<td>';
+            $visor .= '<b style="border: 1px solid; display: block; padding: 3px; border-radius: 2px; background-color: #ffffff;">' . $value->getPresenter()->getDisplayValue() . '</b>';
+            $visor .= '<small>';
+            $visor .= $value->getSection()->getResource()->getSource()->getDisplayName();
+            $visor .= ' ' . $value->getSection()->getPresenter()->getEffectiveAt();
+            $visor .= '</small></td>';
             $visor .= '</tr>';
         }
         $visor .= '</table>';
+
+        $visor .= '<div class="realm-debug">';
+        $visor .= '<b>Concept ID</b>: <a href="#" onclick="window.open(\'https://dataset.perinatologie.nl/peri22x/concepts/' . $conceptId . '\')">' . $conceptId . '</a><br />';
+        $concept = $this->getConcept($conceptId);
+        if ($concept) {
+            $visor .= '<b>Short name</b>: ';
+            $visor .= $concept->getShortName() . '<br />';
+            $visor .= '<b>Label</b>: ';
+            $visor .= $concept->getPresenter()->presentLabel() . '<br />';
+        }
+
+        foreach ($values as $value) {
+            if ($value) {
+                $visor .= 'Value: ' . $value->getValue() . '<br />';
+                $visor .= 'Display: ' . $value->getPresenter()->getDisplayValue() . '<br />';
+                $visor .= 'Resolved: ' . $value->getPresenter()->resolve() . '<br />';
+            } else {
+                $visor .= 'Value: null<br />';
+            }
+            $visor .= '<hr />';
+        }
+
+        $visor .= '</div>';
+
         $visor .= '</div>';
         if (!$htmlValue) {
             $htmlValue = '&#8203;'; // way to enforce minimum 1 character hight spans
@@ -106,7 +111,7 @@ class FusionPresenter extends BasePresenter
             return '<dt>?' . $conceptId . '?</dt><dd>' . $this->presentValueByConceptId($conceptId) . '</dd>';
         }
         if ($label == '') {
-            $label = $concept->getPresenter()->presentLabel();
+            $label = $concept->getPresenter()->presentLabel()   ;
         }
         $html = '';
         $html .= '<dt>' . $label;
